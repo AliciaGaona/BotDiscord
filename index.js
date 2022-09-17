8e36f88e7c63e2ff8c21248e8e64e4bc36bdd1fd
Diti Devs <aligaovela@gmail.com>
2022-09-16 20:57:19 -0500

    Agregando código a index.js, para probar forma #2, comentado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("./node_modules/@discordjs"); //se hace referencia a la dependencia de discord intalada anteriormente
 const config = require("./config.json"); //referencia a config, donde esta el token
 
-const client = new Discord.Client(); //objeto discord cliente
+const client = new Discord.Client(); //objeto discord cliente (bot discord)
+// Este cliente es en parte cómo interactúa con la API de Discord y cómo le notificará eventos como mensajes nuevos.
 
 client.login(config.BOT_TOKEN);//a ese cliente le paso el token
